8375a8766f525a52f4b1c5171e4f61c211d4ecae
Vovas <koweb93@gmail.com>
2025-10-14 01:06:47 +0300

    ger

--- dev/js/all.js
+++ dev/js/all.js
@@ -460,7 +460,7 @@ document.addEventListener('DOMContentLoaded', function () {
       } 
       // Активная точка — с картинкой
       else if (i === activeIndex) {
-        point.style.background = 'url(/img/big-dot.svg) no-repeat center center';
+        point.style.background = 'url(../img/big-dot.svg) no-repeat center center';
         point.style.width = '16px';
         point.style.height = '16px';
       } 
